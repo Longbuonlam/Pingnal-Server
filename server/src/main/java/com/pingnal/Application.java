dafd93f375c1aba01bbb3374cf925273731aa91e
Long <mac@MacBookPro.lan>
2025-07-11 12:35:27 +0700

    rename project

--- server/src/main/java/com/pingnal/Application.java
+++ server/src/main/java/com/pingnal/Application.java
@@ -1,4 +1,4 @@
-package com.pignal;
+package com.pingnal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- server/src/test/java/com/pingnal/ApplicationTests.java
+++ server/src/test/java/com/pingnal/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pignal;
+package com.pingnal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
