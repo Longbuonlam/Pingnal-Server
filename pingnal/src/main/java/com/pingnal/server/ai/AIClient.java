0a73dd4936a061acd6b2a4370994df17b523d68a
Long <mac@MacBookPro.lan>
2025-08-16 11:57:56 +0700

    set up AI Client

---
+++ pingnal/src/main/java/com/pingnal/server/ai/AIClient.java
@@ -0,0 +1,23 @@
+package com.pingnal.server.ai;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+
+public class AIClient {
+
+    public static String GEMINI_API_KEY;
+
+    public static void main(String[] args) {
+
+        Client client = Client.builder().apiKey(GEMINI_API_KEY).build();
+
+        GenerateContentResponse response =
+                client.models.generateContent(
+                        "gemini-2.5-flash",
+                        "Explain how AI works in a few words",
+                        null);
+
+        System.out.println(response.text());
+
+    }
+}
